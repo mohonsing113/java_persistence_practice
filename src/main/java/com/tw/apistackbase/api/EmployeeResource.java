f5a96d56a07aec89543f053b7f98c00a98826dd0
Zhen She <neal.seah@gmail.com>
2018-11-29 05:26:55 +0800

    refactor: introduce jpa

--- src/main/java/com/tw/apistackbase/api/EmployeeResource.java
+++ src/main/java/com/tw/apistackbase/api/EmployeeResource.java
@@ -2,10 +2,10 @@ package com.tw.apistackbase.api;
 
 import com.tw.apistackbase.core.Employee;
 import com.tw.apistackbase.application.EmployeeService;
+import com.tw.apistackbase.core.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.logging.Logger;
 
 @RestController
@@ -14,15 +14,15 @@ public class EmployeeResource {
     private final Logger log = Logger.getLogger(this.getClass().getName());
 
     @Autowired
-    private EmployeeService employeeService;
+    private EmployeeRepository employeeRepository;
 
     @GetMapping(produces = {"application/json"})
-    public List<Employee> list() {
-        return employeeService.list();
+    public Iterable<Employee> list() {
+        return employeeRepository.findAll();
     }
 
     @PostMapping(produces = {"application/json"})
     public void add(@RequestBody Employee employee) {
-        employeeService.add(employee);
+        employeeRepository.save(employee);
     }
 }
